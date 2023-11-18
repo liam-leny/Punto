5cd75f83fe2439264f9248b12d643766a3e6112e
liam-leny <liamleny@protonmail.com>
2023-11-18 11:20:54 +0100

    Card placement condition and check color series

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -4,12 +4,12 @@ import "./Cards.css"
 import Lignes from "./Lignes";
 
 function GameBoard(props) {
-  const [plateau, setPlateau] = useState(new Array(6).fill(new Array(6)));
+  const [plateau, setPlateau] = useState(new Array(6).fill([]).map(() => []));
   const [playerNumber] = useState(props.players.length);
   const [cards, setCards] = useState(new Array(playerNumber));
   const [currentCard, setCurrentCard] = useState(new Array(2).fill([]).map(() => []));
   const [colors, setColors] = useState(['red', 'yellow', 'blue', 'green']);
-  const [playerTurn, setplayerTurn] = useState(0);
+  const [playerTurn, setPlayerTurn] = useState(0);
   const [cardsDistributionComplete, setCardsDistributionComplete] = useState(false);
 
 
@@ -99,14 +99,112 @@ function GameBoard(props) {
   }
 
   /**
+   * Permet de savoir si il y a un vainqueur
+   * @param {*} x Coordonnée x de la dernière carte posée
+   * @param {*} y Coordonnée y de la dernière carte posée
+   * @returns false si il n'y a pas de gagnant, true sinon
+   */
+  function checkVictory(x, y) {
+    const currentColor = currentCard[0];
+    let nbCardsSeries = 4;
+    if (playerNumber === 2) {
+      nbCardsSeries = 5
+    }
+
+    // Vérifier la ligne
+    let rowCount = 1;
+    for (let i = y - 1; i >= 0 && plateau[x][i] !== undefined && plateau[x][i][0] === currentColor; i--) {
+      rowCount++;
+    }
+    for (let i = y + 1; i < plateau[x].length && plateau[x][i] !== undefined && plateau[x][i][0] === currentColor; i++) {
+      rowCount++;
+    }
+    if (rowCount >= nbCardsSeries) {
+      console.log('true')
+      return true;
+    }
+
+    // Vérifier la colonne
+    let columnCount = 1;
+    for (let i = x - 1; i >= 0 && plateau[i][y] !== undefined && plateau[i][y][0] === currentColor; i--) {
+      columnCount++;
+    }
+    for (let i = x + 1; i < plateau.length && plateau[i][y] !== undefined && plateau[i][y][0] === currentColor; i++) {
+      columnCount++;
+    }
+    if (columnCount >= nbCardsSeries) {
+      console.log('true')
+      return true;
+    }
+
+    // Vérifier la diagonale principale (de gauche à droite)
+    let mainDiagonal = 1;
+    for (let i = 1; i < nbCardsSeries; i++) {
+      const newRow = x - i;
+      const newCol = y - i;
+      if (newRow >= 0 && newCol >= 0 && plateau[newRow][newCol] !== undefined && plateau[newRow][newCol][0] === currentColor) {
+        mainDiagonal++;
+      } else {
+        break;
+      }
+    }
+    for (let i = 1; i < nbCardsSeries; i++) {
+      const newRow = x + i;
+      const newCol = y + i;
+      if (newRow < plateau.length && newCol < plateau.length && plateau[newRow][newCol] !== undefined && plateau[newRow][newCol][0] === currentColor) {
+        mainDiagonal++;
+      } else {
+        break;
+      }
+    }
+    console.log('mainDiagonal', mainDiagonal)
+    if (mainDiagonal >= nbCardsSeries) {
+      console.log('true')
+      return true;
+    }
+
+    // Vérifier la diagonale secondaire (de droite à gauche)
+    let secondaryDiagonal = 1;
+    for (let i = 1; i < nbCardsSeries; i++) {
+      const newRow = x - i;
+      const newCol = y + i;
+      if (newRow >= 0 && newCol < plateau.length && plateau[newRow][newCol] !== undefined && plateau[newRow][newCol][0] === currentColor) {
+        secondaryDiagonal++;
+      } else {
+        break;
+      }
+    }
+    for (let i = 1; i < nbCardsSeries; i++) {
+      const newRow = x + i;
+      const newCol = y - i;
+      if (newRow < plateau.length && newCol >= 0 && plateau[newRow][newCol] !== undefined && plateau[newRow][newCol][0] === currentColor) {
+        secondaryDiagonal++;
+      } else {
+        break;
+      }
+    }
+    console.log('secondaryDiagonal', secondaryDiagonal)
+    if (secondaryDiagonal >= nbCardsSeries) {
+      console.log('true')
+      return true;
+    }
+
+    console.log('false')
+    return false
+  }
+
+  const handlePlayerTurnChange = () => {
+    setPlayerTurn(playerTurn + 1);
+  };
+
+  /**
    * Permet d'afficher le plateau 
    * @returns Un tableau contenant toutes les lignes du plateau
    */
   const affichagePlateau = () => {
-    console.log(props.players)
     let rows = [];
     for (let i = 0; i < 6; i++) {
-      rows.push(<Lignes key={i} i={i} ligneTableau={plateau[i]} tT={6} currentCard={currentCard} />)
+      rows.push(<Lignes key={i} i={i} tT={6} currentCard={currentCard} plateau={plateau} setPlateau={setPlateau} checkVictory={checkVictory} playerTurn={playerTurn} handlePlayerTurnChange={handlePlayerTurnChange} />)
     }
     return rows;
   }
@@ -117,10 +215,9 @@ function GameBoard(props) {
    */
   const affichageCards = () => {
     console.log(cards)
-    const c = cards[playerTurn][0]
+    const c = cards[0][0]
     setCurrentCard(c)
   }
-
 
   return (
     <div className="container">
--- src/Lignes.js
+++ src/Lignes.js
@@ -6,10 +6,9 @@ import "./GameBoard.css"
 function Lignes(props) {
 
   const generationCases = () => {
-
     let cases = [];
     for (let numCase = 0; numCase < props.tT; numCase++) {
-      cases.push(<Cases key={numCase} i={props.i} j={numCase} tT={props.tT} currentCard={props.currentCard}/>)
+      cases.push(<Cases key={numCase} i={props.i} j={numCase} tT={props.tT} currentCard={props.currentCard}  plateau={props.plateau} setPlateau={props.setPlateau} checkVictory={props.checkVictory} playerTurn={props.playerTurn} handlePlayerTurnChange={props.handlePlayerTurnChange} />)
     }
     return cases;
   }
